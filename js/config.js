4adf68d3bdbc454f6129b29b89d904a9ccea22b9
Dan Orr <danielk.orr@gmail.com>
2026-08-31 17:36:41 -0700

    Make Research Lab bridges local-preview aware

--- js/config.js
+++ js/config.js
@@ -24,4 +24,6 @@ window.VIA_CONFIG = {
 //  Kept here (shared config) so both generated pages agree on the URLs.
 window.VIA_ROMAN_URL     = 'https://danielkorr.github.io/ancient-world-explorer/';
 window.VIA_ALEXANDER_URL = 'https://danielkorr.github.io/ancient-world-explorer/alexander-the-great-campaigns/';
-window.VIA_RESEARCH_LAB_URL = 'https://danielkorr.github.io/ancient-world-explorer/research-lab/';
+window.VIA_RESEARCH_LAB_URL = /^(localhost|127\.0\.0\.1)$/.test(location.hostname)
+  ? location.origin + '/research-lab/'
+  : 'https://danielkorr.github.io/ancient-world-explorer/research-lab/';
